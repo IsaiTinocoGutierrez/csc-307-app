cbe902f563b674b042acd96ac29a2354441a1105
Isai Tinoco <itinocog@calpoly.edu>
2024-10-14 14:55:27 -0700

    step 1 and 2 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,18 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -22,6 +30,11 @@ function MyApp() {
     setPerson({ name: "", job: "" });
   }
 
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
   return (
     <div className="container">
       <Table
@@ -32,4 +45,6 @@ function MyApp() {
     </div>
   );  
 }
+
+
 export  default MyApp;
